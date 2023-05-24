55f3abb702e597d62624177a4d374c7bb36259f7
Angeles <Angeles@DESKTOP-887BFQD>
2023-05-24 12:16:04 +0200

    basico

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+
+import logoImg from "./content/logo_1.png";
+import "./mystyles.scss";
+
+
+console.log("Hola!");
+
+
+const img = document.createElement("img");
+img.src = logoImg;
+
+
+document.getElementById("root").appendChild(img);
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
+module.exports = {
+  context: path.resolve(__dirname, "./src"),
+  entry: {
+    app: "./index.ts",
+  },
+  output: {
+    filename: "[name].[chunkhash].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  devServer: {
+    port: 8080,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+      filename: "index.html",
+      scriptLoading: "blocking",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
